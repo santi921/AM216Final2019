b66257b9293b09d5592fe97c14aa906705342013
Nicholas Cuccia <cuccia.nicholas@gmail.com>
2019-04-25 02:01:29 -0400

    Updates to CNN
    Changes aspects of network,
    
    Made data import work.

--- divider.py
+++ divider.py
@@ -1,8 +1,8 @@
 import csv
 import numpy as np 
-import pandas as pd
 import os, os.path
 import re
+from joblib import Parallel, delayed 
 
 def single_folder(block_num, exp_num, csv_dir = ''):
     """
@@ -14,81 +14,82 @@ def single_folder(block_num, exp_num, csv_dir = ''):
     csv_dir: (str) Directory which holds the original files.
     """
     #Define a shorthand directory
-    if csv_dir:
-        sub_dir = (csv_dir + 'ML_block' 
-                    + str(block_num) 
-                    + '_Exp'+str(exp_num))
-    else:
-        sub_dir = (csv_dir + 'ML_block' 
+    sub_dir = (csv_dir + 'ML_block' 
                     + str(block_num) 
                     + '_Exp'+str(exp_num))
     
+    exp_dir = 'ML_block' + str(block_num) + '_Exp'+ str(exp_num) 
+        
     #Obtain time-values
-    time  =  np.genfromtxt(sub_dir + '/Time/T_ML_block'
-                + str(block_num) + '_Exp'+ str(exp_num) + '.csv', delimiter=',')
+    time  =  np.genfromtxt(os.path.join(sub_dir, 'Time', 'T_' + exp_dir + '.csv')
+                           , delimiter=',')
 
     #Obtain shear-values
-    shear =  np.genfromtxt(sub_dir + '/Shear/Fs_ML_block'
-                + str(block_num) + '_Exp' + str(exp_num) + '.csv', delimiter=',')
+    shear =  np.genfromtxt(os.path.join(sub_dir, 'Shear', 'Fs_' + exp_dir + '.csv')
+                           , delimiter=',')
 
     #Obtain normal-values
-    normal =  np.genfromtxt(sub_dir + '/Normal/Fn_ML_block'
-                + str(block_num)+'_Exp' + str(exp_num) + '.csv', delimiter=',')
+    normal =  np.genfromtxt(os.path.join(sub_dir, 'Normal', 'Fn_' + exp_dir + '.csv')
+                            , delimiter=',')
     
     #Create a directory for cropped images (if one doesn't exist)
+    if not os.path.exists(os.path.join(csv_dir, 'sub_images', exp_dir)):
+        os.makedirs(os.path.join(csv_dir, 'sub_images', exp_dir))
     
-    if not os.path.exists(sub_dir + "/sub_images/Cropped/"):
-        os.mkdir(sub_dir + "/sub_images/Cropped/")
-#         os.mkdir("./sub_images"+ "/exp" + str(exp_num) + "_block"+ str(block_num))
-
     stats = []
-    labels = []
-
+    
+    onlyfiles = [f for f in os.listdir(sub_dir +'/Contact') 
+                 if os.path.isfile(os.path.join(sub_dir +'/Contact', f))]
+    
     #Go through all images from each experiment and crop them into a usable form
-    #i starts at -1 to account for the incrementing factor in the for loop.
-    i = -1
-    for file in os.listdir(sub_dir + '/Contact'):
-        file_dir = sub_dir + '/Contact/' + file
-        #Check to make-sure the path is a file, not a directory
-        if not os.path.isdir(file_dir):
-            #Obtain raw-image 2-D array
-            raw_image =  np.genfromtxt(file_dir, delimiter=',')
-            #Only increment counter if we are looking at a file.
-            i +=1
+    for file in onlyfiles:
+        #Obtain index value from current file
+        #Changes image index to start at 0, instead of 1
+        print(file)
+        ind = int(re.findall(r'\d+', file)[0]) - 1
 
-        #Extract label values 
-        time_val   = time[i]
-        shear_val  = shear[i]
-        normal_val = normal[i]
-        labels.append([i,time_val, shear_val, normal_val])
+        #Obtain values from array
+        time_val = time[ind]
+        shear_val = shear[ind]
+        normal_val = normal[ind]
+        stats.append([ind, time_val, shear_val, normal_val])
         
-        data = pd.read_csv('ML_block1_Exp201/Contact/image_1_ML_block1_Exp201.csv', header = None).values
-
-        #pd.read_csv('ML_block'+ str(block_num) +'_Exp'+ str(exp_num) +'/Contact/' + file, header = None)
-
-        dim = ((np.asarray(data.shape)+1)/2).astype('int')
-        data = data[dim[0]-750:dim[0]+750, dim[1]-750:dim[1]+750]
-
+        #Obtain contact data
+        contact_data = np.genfromtxt(os.path.join(sub_dir, 'Contact', file)
+                                     , delimiter=',')
+        
+        #Obtain shape of images for cropping
+        dim = ((np.asarray(contact_data.shape)+1)/2).astype('int')
+        contact_data = contact_data[dim[0]-750:dim[0]+750, dim[1]-750:dim[1]+750]
+        
+        #Make subimages
         for i in range(15):
             for j in range(15):
-                temp = data[(i)*100:(i+1)*100, j*100:(j+1)*100]
-                fname = "./sub_images"+ "/exp" + str(exp_num) + "_block" + str(block_num) + "/" + 'image' + str(ind) + 'x' + str(i) + 'y' + str(j) + ".csv"
+                temp = contact_data[(i)*100:(i+1)*100, j*100:(j+1)*100]
+                fname = os.path.join(csv_dir, 'sub_images', exp_dir, 
+                                     'image_' + str(ind) + '_x_' + str(i) + '_y_' + str(j) 
+                                     + '_' + exp_dir + ".csv")
                 np.savetxt(fname, temp, delimiter=',', newline='\n',fmt='%i')
-
+                
     stats_numpy = np.array(stats).reshape(-1,4)
     stats_numpy = stats_numpy[stats_numpy[:,0].argsort()]
-    np.savetxt("./sub_images"+ "/exp" + str(exp_num) + "_block" + str(block_num) + "/folder_stats.csv",stats_numpy , delimiter=",")
-
-
-
+    #Make label file
+    np.savetxt(os.path.join(csv_dir, 'sub_images', exp_dir, 'labels_' + exp_dir + '.csv')
+               , stats_numpy, delimiter=",")
+    
 if __name__ == "__main__":
+    
+    csv_dir = 'D:/MLdata/18-01-09d-Exp/Jedi Data/CSV/'
+    # csv_dir = '.'
 
+    image_folders = [filename for filename in os.listdir(csv_dir) if filename.startswith('ML')]
 
-	image_folders = [filename for filename in os.listdir('.') if filename.startswith('ML')]
+    Parallel(n_jobs=-1)(
+        delayed(single_folder)(re.findall(r'\d+', folders)[0], re.findall(r'\d+', folders)[1], csv_dir) 
+        for folders in image_folders)
 
-	for folders in image_folders:
-	    vals = re.findall(r'\d+', folders)        
-	    exp_num = vals[0]
-	    block_num = vals[1]
-
-	    single_folder(exp_num,block_num)
+#     for folders in image_folders:
+#         vals = re.findall(r'\d+', folders)        
+#         exp_num = vals[1]
+#         block_num = vals[0]
+#         single_folder(block_num, exp_num, csv_dir)
